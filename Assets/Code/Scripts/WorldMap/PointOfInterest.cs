e6c92a876e93f4608c4025f30828e33e4886772c
ZaiR37 <70493871+ZaiR37@users.noreply.github.com>
2023-09-04 09:04:00 +0700

    Adding Point of Interest
    Adding base for point of interest (like town or something) that are connected by roads.

---
+++ Assets/Code/Scripts/WorldMap/PointOfInterest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Dreamteck.Splines;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour{
+
+    [SerializeField] private Transform icon;
+    [SerializeField] private string townName;
+
+    [SerializeField] private Road[] roadList;
+
+    private float iconSizeMin = 0.1f;
+    private float iconSizeMax = 0.25f;
+    private float cameraMaxZoom;
+    private float cameraMinZoom; 
+
+    private void Start() {
+        gameObject.name = townName;
+        cameraMaxZoom = CameraController.Instance.GetMaxZoom();
+        cameraMinZoom = CameraController.Instance.GetMinZoom();
+
+        HadleNearestPointPoisition();
+    }
+
+    private void Update() {
+        HandleIconSize();
+    }
+
+    private void HandleIconSize(){
+        float currentZoom = CameraController.Instance.GetZoomValue();
+        float iconSize = Mathf.Lerp(iconSizeMin, iconSizeMax, Mathf.InverseLerp(cameraMinZoom, cameraMaxZoom, currentZoom));
+        icon.localScale = new Vector3(iconSize, iconSize, iconSize);
+    }
+
+    private void HadleNearestPointPoisition(){
+        if(roadList == null) return;
+
+        foreach (Road road in roadList){
+            road.SetNearestPointPoisition(transform.position);
+        }
+    }
+
+    public Road[] GetRoadList() => roadList;
+}
