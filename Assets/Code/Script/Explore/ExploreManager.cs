443d69d5663ca707d8ce5c83eac1574227d2c5ef
MJS <darkimanity@gmail.com>
2023-10-09 14:32:46 +0700

    Polish dan delete Codemonkey util

--- Assets/Code/Script/Explore/ExploreManager.cs
+++ Assets/Code/Script/Explore/ExploreManager.cs
@@ -5,13 +5,7 @@ namespace Nivandria.Explore
     using UnityEngine;
     using UnityEngine.InputSystem;
     public class ExploreManager : MonoBehaviour
-    {        
-        [SerializeField] InputSystem inputSystem;
-        [SerializeField] PlayerInput playerInput;
-        [SerializeField] Transform slot_leader;
-        [SerializeField] GameObject panel_party;
-        [SerializeField] Transform players;
-        [SerializeField] Animator transisi;
+    {
         Vector3[] last_posisi = new Vector3[]
             {
             new Vector3(-4.7f, 1f, -11f), //Posisi dari luar ke dalam rumah
@@ -22,11 +16,19 @@ namespace Nivandria.Explore
 
            // new Vector3(25f, 1.8f, 15f) //Posisi dari outskrit ke halaman rumah
             };
+
+        [SerializeField] InputSystem inputSystem;
+        [SerializeField] PlayerInput playerInput;
+        [SerializeField] Transform slot_leader;
+        [SerializeField] GameObject panel_party;
+        [SerializeField] Transform players;
+        [SerializeField] Animator transisi;
+        
         private static ExploreManager instance;
         private int set_lead;
         public static int set_lastScene;
         public int GetLead() => set_lead;
-        
+
         private void Awake()
         {
             if (instance != null)
@@ -48,45 +50,41 @@ namespace Nivandria.Explore
         // Update is called once per frame
         void Update()
         {
-            switch (set_lastScene)
-            {
-                case 1:                    
-                    SwitchPosition(0);
-                    Invoke(nameof(KembaliKeNol), 0.5f);
-                    break;
-                case 2:
-                    SwitchPosition(1);
-                    Invoke(nameof(KembaliKeNol), 0.5f);
-                    break;
-                case 3:
-                    SwitchPosition(2);
-                    Invoke(nameof(KembaliKeNol), 0.5f);
-                    break;
-                case 4:
-                    SwitchPosition(3);
-                    Invoke(nameof(KembaliKeNol), 0.5f);
-                    break;
-            }
-
-            Debug.Log(set_lastScene);
+            ChangeTransisi();
         }
+        /// <summary>
+        /// Mengubah Index Sesuai Vector3 Array
+        /// </summary>
+        /// <param name="indexarray"></param>
         void SwitchPosition(int indexarray)
         {
             players.position = last_posisi[indexarray];
         }
+        /// <summary>
+        /// Mengatur Kembali Nilai Transisi Ke 0 Agar Tidak Terus Menerus Mengeksekusi Di ChangeTransisi
+        /// </summary>
         void KembaliKeNol()
         {
             set_lastScene = 0;
         }
+        /// <summary>
+        /// Open Panel Party
+        /// </summary>
         public void PanelParty()
         {
             panel_party.SetActive(true);
         }
-        public void ClosePanelParty()
+        /// <summary>
+        /// Close Panel Party
+        /// </summary>
+        void ClosePanelParty()
         {
             panel_party.SetActive(false);
         }
-        public void SetUp()
+        /// <summary>
+        /// Untuk Mengatur Setup Party Secara Manual Dalam Explore 
+        /// </summary>
+        void SetUp()
         {
             string clone = "(Clone)";
             if (slot_leader.GetChild(0).gameObject.name == "Sacra" + clone)
@@ -120,6 +118,31 @@ namespace Nivandria.Explore
             else
             {
                 Debug.Log("Belum Ada Leader");
+            }
+        }
+        /// <summary>
+        /// Untuk Mengatur Posisi Pada Scene Transisi
+        /// </summary>
+        void ChangeTransisi()
+        {
+            switch (set_lastScene)
+            {
+                case 1:
+                    SwitchPosition(0);
+                    Invoke(nameof(KembaliKeNol), 0.5f);
+                    break;
+                case 2:
+                    SwitchPosition(1);
+                    Invoke(nameof(KembaliKeNol), 0.5f);
+                    break;
+                case 3:
+                    SwitchPosition(2);
+                    Invoke(nameof(KembaliKeNol), 0.5f);
+                    break;
+                case 4:
+                    SwitchPosition(3);
+                    Invoke(nameof(KembaliKeNol), 0.5f);
+                    break;
             }
         }
     }
