b4482d4bb43dd008841794146233a13e3fcf2ea4
none <fidelwo05@gmail.com>
2023-12-20 03:15:54 +0700

    Adding ButtonMenuHover
    Menambahkan program untuk hovering button menu

--- Assets/Code/Scripts/UI/Menu/MenuManager.cs
+++ Assets/Code/Scripts/UI/Menu/MenuManager.cs
@@ -5,6 +5,7 @@ namespace Nivandria.UI.Menu
     using TMPro;
     using UnityEngine;
     using UnityEngine.UI;
+    using UnityEngine.EventSystems;
 
     public class MenuManager : MonoBehaviour
     {
@@ -64,6 +65,12 @@ namespace Nivandria.UI.Menu
             SetButtonPropertiesBasedOnName(newMenuButton, "SETTINGS", 46.77f, 45f, Vector2.zero, Vector2.zero);
             SetButtonPropertiesBasedOnName(newMenuButton, "ARCHIEVE", 57.93f, 45f, new Vector2(10f, 0f), new Vector2(-10f, 0f));
             SetButtonPropertiesBasedOnName(newMenuButton, "MAIN MENU", 47.72f, 45f, Vector2.zero, Vector2.zero);
+
+            // Add the hover events
+            AddHoverEvents(newMenuButton, 2);
+            AddHoverEvents(newMenuButton, 4);
+            AddHoverEvents(newMenuButton, 5);
+            AddHoverEvents(newMenuButton, 6);
         }
 
         private void SetIconButtonSize(Transform buttonTransform, float width, float height)
@@ -90,15 +97,62 @@ namespace Nivandria.UI.Menu
 
             if (nameButton.text.Trim().ToUpper() == targetName)
             {
-                // Set size for both left and right icon
+                // Set size for both left and right buttons
                 SetIconButtonSize(newMenuButton.transform.GetChild(0), width, height);
                 SetIconButtonSize(newMenuButton.transform.GetChild(1), width, height);
 
-                // Set position for left and right icon
-                SetIconButtonPosition(newMenuButton.transform.GetChild(0), offsetLeft);
-                SetIconButtonPosition(newMenuButton.transform.GetChild(1), offsetRight);
+                // Get the current anchored position
+                Vector2 currentPositionLeft = newMenuButton.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition;
+                Vector2 currentPositionRight = newMenuButton.transform.GetChild(1).GetComponent<RectTransform>().anchoredPosition;
+
+                // Set position for left and right buttons
+                SetIconButtonPosition(newMenuButton.transform.GetChild(0), currentPositionLeft + offsetLeft);
+                SetIconButtonPosition(newMenuButton.transform.GetChild(1), currentPositionRight + offsetRight);
             }
         }
+
+        private void AddHoverEvents(GameObject newMenuButton, int index)
+        {
+            EventTrigger trigger = newMenuButton.transform.GetChild(index).GetComponent<EventTrigger>();
+            if (trigger == null)
+            {
+                trigger = newMenuButton.AddComponent<EventTrigger>();
+            }
+
+            // Add PointerEnter event
+            EventTrigger.Entry pointerEnter = new EventTrigger.Entry();
+            pointerEnter.eventID = EventTriggerType.PointerEnter;
+            pointerEnter.callback.AddListener((data) => { OnPointerEnter(newMenuButton, index); });
+            trigger.triggers.Add(pointerEnter);
+
+            // Add PointerExit event
+            EventTrigger.Entry pointerExit = new EventTrigger.Entry();
+            pointerExit.eventID = EventTriggerType.PointerExit;
+            pointerExit.callback.AddListener((data) => { OnPointerExit(newMenuButton, index); });
+            trigger.triggers.Add(pointerExit);
+        }
+
+        private void OnPointerEnter(GameObject newMenuButton, int index)
+        {
+            // Get the CanvasGroup component and set alpha to 1 on hover
+            CanvasGroup canvasGroup = newMenuButton.transform.GetChild(index).GetComponent<CanvasGroup>();
+            if (canvasGroup != null)
+            {
+                canvasGroup.alpha = 1;
+            }
+        }
+
+        private void OnPointerExit(GameObject newMenuButton, int index)
+        {
+            // Get the CanvasGroup component and set alpha back to 0 when not hovered
+            CanvasGroup canvasGroup = newMenuButton.transform.GetChild(index).GetComponent<CanvasGroup>();
+            if (canvasGroup != null)
+            {
+                canvasGroup.alpha = 0;
+            }
+        }
+
+        
     }
 
 }
